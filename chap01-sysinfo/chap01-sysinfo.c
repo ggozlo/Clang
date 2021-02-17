3c59d3f2431a1c6995dd46c675355e89a4262550
ggozlo <ggozlo@naver.com>
2021-02-17 15:02:50 +0900

    chap01

---
+++ chap01-sysinfo/chap01-sysinfo.c
@@ -0,0 +1,25 @@
+﻿// 시스템 정보 확인 
+
+// sizeof(type) - 내장함수(Built-in Function) - 타입의 메모리 점유율 출력 (Byte 단위)
+#include <stdio.h>
+
+int main()
+{
+	printf(">>> system information<<<\n");
+
+	printf("sizeof(char)(%d)\n", sizeof(char));
+	printf("sizeof(short)(%d)\n", sizeof(short));
+	printf("sizeof(int)(%d)\n", sizeof(int));
+	printf("sizeof(long)(%d)\n", sizeof(long));
+	printf("sizeof(float)(%d)\n", sizeof(float));
+	printf("sizeof(double)(%d)\n", sizeof(double));
+
+	printf("sizeof(char*)(%d)\n", sizeof(char*));
+	printf("sizeof(short*)(%d)\n", sizeof(short*));
+	printf("sizeof(int*)(%d)\n", sizeof(int*));
+	printf("sizeof(long*)(%d)\n", sizeof(long*));
+	printf("sizeof(float*)(%d)\n", sizeof(float*));
+	printf("sizeof(double*)(%d)\n", sizeof(double*));
+
+	printf("sizeof(void*)(%d)\n", sizeof(void*));
+}
